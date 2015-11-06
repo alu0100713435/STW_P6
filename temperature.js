40726789d7eaab201bb2d3842a58f63e3ce0c14a
alu01007134351 <noelpadron1415@gmail.com>
2015-11-06 11:19:26 +0000

    arreglando cosas

--- temperature.js
+++ temperature.js
@@ -1,4 +1,4 @@
-"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+/*"use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 
 function Medida(valor, tipo){
     this.valor = valor;
@@ -71,5 +71,59 @@ Temperatura.prototype.calculate = function(temp) {
     converted.innerHTML = "ERROR! Prueba con algo como esto '-4.2C' ";
   }
 }
+
+module.exports = Temperatura;*/
+
+"use strict";
+
+function Medida () {
+  this.valor_;
+  this.tipo_;
+}
+
+function Temperatura () {
+  Medida.call(this);
+}
+
+//Temperatura hereda de Medida
+Temperatura.prototype = new Medida();
+//Getters
+Medida.prototype.get_valor = function(){return this.valor_;}
+Medida.prototype.get_tipo = function(){return this.tipo_;}
+//Setters
+Medida.prototype.set_valor = function(valor){this.valor_ = valor;}
+Medida.prototype.set_tipo = function(tipo){this.tipo_ = tipo;}
+
+
+//Constructor
+Temperatura.prototype.inicializar = function(temp){
+
+  var exp_regular = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fFcC])/;
+  var valor = temp.match(exp_regular);
+
+  if(valor !== null){
+  this.set_valor(parseFloat(valor[1]));
+  this.set_tipo(valor[2]);
+  }
+}
+
+//Conversor
+Temperatura.prototype.conversor = function(){
+
+  if(this.get_valor() === undefined || this.get_tipo() === undefined){
+    return ("ERROR! Prueba con algo como esto '-4.2C'");
+  }
+
+  if(this.get_tipo() === 'C' || this.get_tipo() ==='c'){
+    var result = (this.get_valor()*(9/5))+32;
+    return ("El resultado es " + result + " " + "F");
+  }
+
+  else{
+    var result = (this.get_valor()-32)*(5/9);
+    return ("El resultado es " + result + " " + "C");
+  }
+}
+
 
 module.exports = Temperatura;
